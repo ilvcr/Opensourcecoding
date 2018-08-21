724a141ecb87675c7f25b0c66b10bf73e4bb6606
yoghourt <ilvcr@outlook.com>
2018-08-22 02:25:48 +0800

    2018->08->21

---
+++ leveldbglg/db/builder.cc
@@ -0,0 +1,99 @@
+/*************************************************************************
+> File Name: builder.cc
+> Author: yoghourt->ilvcr 
+> Mail: liyaoliu@foxmail.com  @@  ilvcr@outlook.com 
+> Created Time: 2018年08月21日 星期二 19时25分54秒
+> Description: 
+    Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+    Use of this source code is governed by a BSD-style license that can be
+    found in the LICENSE file. See the AUTHORS file for names of contributors.
+ ************************************************************************/
+
+#include "db/builder.h"
+
+#include "db/filename.h"
+#include "db/dbformat.h"
+#include "db/table_cache.h"
+#include "db/version_edit.h"
+#include "leveldb/db.h"
+#include "leveldb/env.h"
+#include "leveldb/iterator.h"
+
+namespace leveldb{
+    
+    Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+            TableCache* table_cache, Iterator* iter, FileMetaData* meta){
+
+        Status s;
+        meta->file_size = 0;
+        iter->SeekToFirst();
+
+        std::string fname = TableFileName(dbname, meta->number);
+        if(iter->Valid()){
+
+            WritableFile* file;
+            s = env->NewWritableFile(fname, &file);
+            if(!s.ok()){
+
+                return s;
+            }
+
+            TableBuilder* builder = new TableBuilder(options, file);
+            meta->smallest.DecodFrom(iter->key());
+            for( ; iter->Valid(); iter->Next()){
+
+                Slice key = iter->key();
+                meta->largest.DecodeFrom(key);
+                builder->Add(key, iter->value());
+            }
+
+            // Finish and check for builder errors
+            s = builder->Finish();
+            if(s.ok()){
+
+                meta->file_size = builder->FileSize();
+                assert(meta->file_size > 0);
+            }
+            delete builder;
+
+            // Finish and check for file errors
+            if(s.ok()){
+
+                s = file->Sync();
+            }
+
+            if(s.ok()){
+
+                s = file->Close();
+            }
+            delete file;
+            file = nullptr;
+
+            if(s.ok()){
+
+                // Verify that the table is usable
+                Iterator* it = table_cache->NewIterator(ReadOptions(), 
+                                                    meta->number, meta->file_size);
+                s = it->status();
+                delete it;
+            }
+        }
+
+        // Check for input iterator errors
+        if(!iter->status().ok()){
+
+            // keep it
+        }
+        else{
+
+            env->DeleteFile(fname);
+        }
+
+        return s;
+    }
+
+}      namespace leveldb
+
+
+
+
